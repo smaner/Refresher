c356d5c6ad8081c5075ce0a76835f5e5ea4400e6
smaner <bobzorxo@gmail.com>
2021-08-29 19:31:09 -0500

    Added docs to TankFactory

--- refresher/src/TankFactory/TankFactory.java
+++ refresher/src/TankFactory/TankFactory.java
@@ -1,7 +1,9 @@
 package TankFactory;
 
 public class TankFactory {
-    
+    /** Static Factory Method for building tanks. Options: HEAVYTANK, MEDIUMTANK,
+     * SELFPROPELLEDGUN, TANKDESTROYER, LIGHTTANK.
+     */
     public static Tank getTank(TankType type) {
         return type.getConstructor().get();
       }
